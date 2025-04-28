e495aa1abea7beb58c03fceffb594a536fe86a95
ekusiadadus <ekusiadadus@gmail.com>
2025-04-28 21:58:44 +0900

    feat: init

---
+++ main.py
@@ -0,0 +1,19 @@
+import qrcode
+
+# Create a QR code instance
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# Add data to the QR code
+qr.add_data('https://breakai.ai')
+qr.make(fit=True)
+
+# Create an image from the QR code
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image
+img.save("example_qr.png")
